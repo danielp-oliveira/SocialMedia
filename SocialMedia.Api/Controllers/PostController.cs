94c17c0e9a5eadfcf4133f85922d44403bca6561
danielp-oliveira <danielp.oliveira@outlook.com>
2020-07-10 23:30:34 -0300

    Fluent Api in EF Core

--- SocialMedia.Api/Controllers/PostController.cs
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -15,11 +15,18 @@ namespace SocialMedia.Api.Controllers
             _postRepository = postRepository;
         }
 
-        [HttpGet]
+        [HttpGet("")]
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
             return Ok(posts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPost(int id)
+        {
+            var post = await _postRepository.GetPost(id);
+            return Ok(post);
         }
     }
 }
--- SocialMedia.Infrastructure/Repositories/PostRepository.cs
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -16,11 +16,16 @@ namespace SocialMedia.Infrastructure.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<Publicacion>> GetPosts()
+        public async Task<IEnumerable<Post>> GetPosts()
         {
-            var posts = await _context.Publicacion.ToListAsync();
-
+            var posts = await _context.Posts.ToListAsync();
             return posts;
+        }
+
+        public async Task<Post> GetPost(int id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            return post;
         }
     }
 }
