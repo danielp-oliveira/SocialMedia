eb822eb5accfac3e76acdbda84eb16648bae2e66
Daniel Oliveira <daniel.oliveira@fariasbrito.com.br>
2020-07-10 21:23:45 -0300

    Dependency Injection & EF Core

---
+++ SocialMedia.Api/Controllers/PostController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPosts()
+        {
+            var posts = await _postRepository.GetPosts();
+            return Ok(posts);
+        }
+    }
+}
